df9a34aecd3c76088e2b9bfdf94881ed9638cfd7
Batandwa Mgutsi <B.mgutsi@gmail.com>
2021-01-21 21:57:59 +0200

    Adds library

---
+++ lib/npxl_video.dart
@@ -0,0 +1,13 @@
+library npxl_video;
+
+export 'src/byte_output_stream.dart';
+export 'src/in_memory_byte_output_stream.dart';
+export 'src/in_memory_random_access_byte_input_stream.dart';
+export 'src/media_page_builder.dart';
+export 'src/numbers.dart';
+export 'src/random_access_byte_input_stream.dart';
+export 'src/readers.dart';
+export 'src/revision.dart';
+export 'src/stream_utils.dart';
+export 'src/video_builder.dart';
+export 'src/video_resource_builder.dart';
