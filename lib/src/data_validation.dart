7d22678834fe0175f6337fcc871d1e0ab912a9e2
Batandwa Mgutsi <B.mgutsi@gmail.com>
2021-01-21 02:05:14 +0200

    Adds checksum algorithm

--- lib/src/data_validation.dart
+++ lib/src/data_validation.dart
@@ -1,6 +1,8 @@
 import 'dart:typed_data';
+import 'package:crclib/crclib.dart';
+import 'package:crclib/catalog.dart';
 
+/// Computes a CRC16/ARC checksum for the given data
 int computeCRC16Checksum(Uint8List data) {
-  // TODO(LytOwl Team): Implement
-  return data.length;
+  return int.parse(Crc16Arc().convert(data.toList()).toString());
 }
